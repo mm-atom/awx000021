15cd524a11e6b51cce128d4f2324cf4fb225a83c
taoqf <tao_qiufeng@126.com>
2020-04-03 11:28:50 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+import global from '@mmstudio/global';
 
-export default function awx21(param1: string) {
+export default function download_file(url: string) {
+	return new Promise<string>((resolve, reject) => {
+		if (!/^https?/.test(url)) {
+			const host = global('host', 'http://127.0.0.1:8889');
+			url = `${host}/fsweb/getfile?id=${url}`;
+		}
+		wx.downloadFile({
+			url,
+			success(res) {
+				resolve(res.tempFilePath);
+			},
+			fail(res) {
+				reject(res);
+			}
+		});
+	});
 }
